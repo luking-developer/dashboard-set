a1943b5df1f9ee4dced180d585f00f243fe61640
Lucas Zurverra <luking.dev@gmail.com>
2026-01-08 15:08:08 +0000

    Change rural id

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ if uploaded_file is not None:
             next_urbano = max(urbano_sets) + 1 if urbano_sets else 10000000
             
             # Rural
-            rural_sets = [int(s) for s in valid_sets if s[-4] == '5']
+            rural_sets = [int(s) for s in valid_sets if s[-4] >= '5']
             next_rural = max(rural_sets) + 1 if rural_sets else 50000000
             
             free_sets = st.subheader("Números de SET disponibles", anchor="free_sets")
